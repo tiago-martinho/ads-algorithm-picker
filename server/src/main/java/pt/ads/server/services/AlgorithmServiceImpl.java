421535ebb5bda26b2e832a35f81039647bfcb26b
Sandro Marques <sandro123iv@gmail.com>
2020-10-03 13:52:35 +0100

    Load the OWL ontology from the classpath.

--- server/src/main/java/pt/ads/server/services/AlgorithmServiceImpl.java
+++ server/src/main/java/pt/ads/server/services/AlgorithmServiceImpl.java
@@ -1,25 +1,37 @@
 package pt.ads.server.services;
 
+import java.io.IOException;
+
 import lombok.extern.slf4j.Slf4j;
 import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
-
-import java.io.File;
 
 @Service
 @Slf4j
 public class AlgorithmServiceImpl implements AlgorithmService {
 
-    private static final String ONTOLOGY_PATH = "src/main/resources/PMOEA.owl";
+    @Value("classpath:PMOEA.owl")
+    private Resource owlFile;
+
 
     @Override
-    public Object getAlgorithm() throws OWLOntologyCreationException {
+    public Object getAlgorithm() throws OWLOntologyCreationException, IOException {
         OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-        OWLOntology ontology = manager.loadOntologyFromOntologyDocument(new File(ONTOLOGY_PATH));
-        log.debug(String.valueOf(ontology));
+        OWLOntology ontology = manager.loadOntologyFromOntologyDocument(owlFile.getInputStream());
+        OWLClass clazz = manager.getOWLDataFactory().getOWLThing();
+
+        log.debug("Ontology Loaded...");
+        log.debug("Document IRI: " + owlFile);
+        log.debug("Ontology    : " + ontology.getOntologyID());
+        log.debug("Format      : " + manager.getOntologyFormat(ontology));
+        log.debug("Thing Class : " + clazz);
+
         return null;
     }
 
